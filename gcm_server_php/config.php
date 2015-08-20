d1469f72a2c3d448f34135f09b67ed25d68b6810
Jenishgithub <jenish_shakya33@yahoo.com>
2015-08-20 08:37:08 -0800

    php server files added

---
+++ gcm_server_php/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "gcm");
+
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyAbYANr0AG11mXWQXthf73nxg6upjvtmAQ"); // Place your Google API Key (key for browser applicaltion not server application)
+?>
